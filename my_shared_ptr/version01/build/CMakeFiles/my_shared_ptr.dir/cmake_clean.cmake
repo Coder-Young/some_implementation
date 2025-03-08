0ebae9ec30200701f3be0ec3071c8afef0bbd319
yzq <1561844542@qq.com>
2025-03-08 23:05:25 +0800

    nul

---
+++ my_shared_ptr/version01/build/CMakeFiles/my_shared_ptr.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_shared_ptr.dir/src/main.cpp.o"
+  "CMakeFiles/my_shared_ptr.dir/src/main.cpp.o.d"
+  "my_shared_ptr"
+  "my_shared_ptr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_shared_ptr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
